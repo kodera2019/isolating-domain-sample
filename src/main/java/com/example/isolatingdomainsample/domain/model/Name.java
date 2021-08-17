72d8962dc0c16ec36f8169a9e0c8d2b009da3cd9
kodera2019 <koderayuta@team-lab.com>
2021-08-17 23:00:25 +0900

    domainにNameの値オブジェクト追加

---
+++ src/main/java/com/example/isolatingdomainsample/domain/model/Name.java
@@ -0,0 +1,28 @@
+package com.example.isolatingdomainsample.domain.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * 氏名
+ */
+public class Name {
+
+  final int 字数制限 = 40;
+
+  @NotBlank(message = "名前を入力してください。")
+  @Size(max = 字数制限, message = "{max}文字以内で入力してください。")
+  private String value = "";
+
+  public Name() {
+  }
+
+  public Name(String name) {
+    this.value = name;
+  }
+
+  @Override
+  public String toString() {
+    return value;
+  }
+}
