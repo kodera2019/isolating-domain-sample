fea99062887d6e876e2187aa99f1ac0985aa738e
kodera2019 <koderayuta@team-lab.com>
2021-08-17 22:32:50 +0900

    initial commit

---
+++ src/main/java/com/example/isolatingdomainsample/IsolatingDomainSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.isolatingdomainsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsolatingDomainSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsolatingDomainSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/isolatingdomainsample/IsolatingDomainSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.isolatingdomainsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsolatingDomainSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
