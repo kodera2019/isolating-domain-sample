9380f21f0f7bde690fb88b6ab6de18e05d90d38f
kodera2019 <koderayuta@team-lab.com>
2021-08-19 01:46:07 +0900

    EmployeeMapperを作成

---
+++ src/main/java/com/example/isolatingdomainsample/infrastructure/datasource/employee/EmployeeMapper.java
@@ -0,0 +1,11 @@
+package com.example.isolatingdomainsample.infrastructure.datasource.employee;
+
+import com.example.isolatingdomainsample.domain.model.employee.Employee;
+import com.example.isolatingdomainsample.domain.model.employee.EmployeeNumber;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface EmployeeMapper {
+  Employee selectByEmployeeNumber(@Param("employeeNumber")EmployeeNumber employeeNumber);
+}
