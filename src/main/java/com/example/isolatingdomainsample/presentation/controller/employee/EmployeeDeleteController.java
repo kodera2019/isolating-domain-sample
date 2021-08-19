068019382c98646b1c5c326fb60312ec44832f89
kodera2019 <koderayuta@team-lab.com>
2021-08-19 23:12:32 +0900

    EmployeeDeleteControllerを作成

---
+++ src/main/java/com/example/isolatingdomainsample/presentation/controller/employee/EmployeeDeleteController.java
@@ -0,0 +1,33 @@
+package com.example.isolatingdomainsample.presentation.controller.employee;
+
+import com.example.isolatingdomainsample.application.service.employee.EmployeeQueryService;
+import com.example.isolatingdomainsample.application.service.employee.EmployeeRecordService;
+import com.example.isolatingdomainsample.domain.model.employee.Employee;
+import com.example.isolatingdomainsample.domain.model.employee.EmployeeNumber;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 従業員削除
+ */
+@Controller
+@RequestMapping("employee/{employeeNumber/delete}")
+public class EmployeeDeleteController {
+
+  EmployeeQueryService employeeQueryService;
+  EmployeeRecordService employeeRecordService;
+
+  EmployeeDeleteController(EmployeeQueryService employeeQueryService, EmployeeRecordService employeeRecordService) {
+    this.employeeQueryService = employeeQueryService;
+    this.employeeRecordService = employeeRecordService;
+  }
+
+  @GetMapping
+  String deleteThenRedirect(@PathVariable("employeeNumber") EmployeeNumber employeeNumber) {
+    Employee employee = employeeQueryService.choose(employeeNumber);
+    employeeRecordService.expireContract(employee);
+    return "redirect:/employees";
+  }
+}
