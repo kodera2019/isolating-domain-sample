9ef016e48d01e982329b73b3cc5b8ba543092fbb
kodera2019 <koderayuta@team-lab.com>
2021-08-19 23:17:02 +0900

    タイポ修正

--- src/main/java/com/example/isolatingdomainsample/presentation/controller/employee/EmployeeDeleteController.java
+++ src/main/java/com/example/isolatingdomainsample/presentation/controller/employee/EmployeeDeleteController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 従業員削除
  */
 @Controller
-@RequestMapping("employee/{employeeNumber/delete}")
+@RequestMapping("employees/{employeeNumber}/delete")
 public class EmployeeDeleteController {
 
   EmployeeQueryService employeeQueryService;
