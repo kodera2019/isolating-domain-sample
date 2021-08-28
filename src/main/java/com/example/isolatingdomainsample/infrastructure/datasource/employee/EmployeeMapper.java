c480db03f9df116ab250e57854b8cfa2e5d83c2f
kodera2019 <koderayuta@team-lab.com>
2021-08-28 16:00:12 +0900

    EmployeeMapperに名前登録に必要なメソッド追加

--- src/main/java/com/example/isolatingdomainsample/infrastructure/datasource/employee/EmployeeMapper.java
+++ src/main/java/com/example/isolatingdomainsample/infrastructure/datasource/employee/EmployeeMapper.java
@@ -2,6 +2,7 @@ package com.example.isolatingdomainsample.infrastructure.datasource.employee;
 
 import com.example.isolatingdomainsample.domain.model.employee.Employee;
 import com.example.isolatingdomainsample.domain.model.employee.EmployeeNumber;
+import com.example.isolatingdomainsample.domain.model.employee.Name;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -15,9 +16,17 @@ public interface EmployeeMapper {
 
   void insertEmployee(@Param("employeeNumber") EmployeeNumber employeeNumber);
 
+  void deleteEmployeeName(@Param("employeeNumber") EmployeeNumber employeeNumber);
+
+  void insertEmployeeName(@Param("employeeNumber") EmployeeNumber employeeNumber, @Param("nameId") Integer nameId, @Param("name") Name employeeName);
+
+  void insertEmployeeNameHistory(@Param("id") Integer id, @Param("employeeNumber") EmployeeNumber employeeNumber, @Param("name") Name name);
+
   void deleteInspireContract(@Param("employeeNumber") EmployeeNumber employeeNumber);
 
   void insertExpireContract(@Param("employeeNumber") EmployeeNumber employeeNumber);
+
+  Integer newEmployeeNameIdentifier();
 
   Integer newEmployeeNumber();
 }
