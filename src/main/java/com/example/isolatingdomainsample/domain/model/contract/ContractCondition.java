50b8d9ff1738e66754ff3e2534555d4b64724a10
kodera2019 <koderayuta@team-lab.com>
2021-09-12 17:52:06 +0900

    WIP

---
+++ src/main/java/com/example/isolatingdomainsample/domain/model/contract/ContractCondition.java
@@ -0,0 +1,12 @@
+package com.example.isolatingdomainsample.domain.model.contract;
+
+import com.example.isolatingdomainsample.domain.model.contract.shift.Shifts;
+
+/**
+ * 労働条件
+ */
+public class ContractCondition {
+  ContractEffectiveDate effectiveDate;
+  Shifts shifts;
+  // TODO: WageCondition wageCondition;
+}
