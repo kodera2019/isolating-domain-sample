a3f06f9ae3f084718f7cbd90ec750ff5a7af9e64
kodera2019 <koderayuta@team-lab.com>
2021-09-12 17:50:25 +0900

    modelにStartingTime作成

---
+++ src/main/java/com/example/isolatingdomainsample/domain/model/contract/shift/StartingTime.java
@@ -0,0 +1,10 @@
+package com.example.isolatingdomainsample.domain.model.contract.shift;
+
+import com.example.isolatingdomainsample.domain.type.time.Time;
+
+/**
+ * 始業時刻
+ */
+public class StartingTime {
+  Time time;
+}
