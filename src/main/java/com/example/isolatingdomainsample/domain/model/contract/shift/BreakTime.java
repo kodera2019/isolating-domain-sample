60692890d82fcf429b65fdc9d9b5f9c73be5ac34
kodera2019 <koderayuta@team-lab.com>
2021-09-12 17:49:50 +0900

    modelにBreakTime作成

---
+++ src/main/java/com/example/isolatingdomainsample/domain/model/contract/shift/BreakTime.java
@@ -0,0 +1,10 @@
+package com.example.isolatingdomainsample.domain.model.contract.shift;
+
+import com.example.isolatingdomainsample.domain.type.time.Minute;
+
+/**
+ * 休憩時間
+ */
+public class BreakTime {
+  Minute value;
+}
